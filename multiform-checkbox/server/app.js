b7d13e6aa82f3726c28e1eb983d1275dc54e8d84
malikvedanshu <malikvedanshu757@outlook.com>
2022-08-23 12:08:39 +0000

    server side

--- multiform-checkbox/server/app.js
+++ multiform-checkbox/server/app.js
@@ -1,11 +1,15 @@
 import express from "express";
 const app = express();
+import './dbConnect.js';
 const port = 5000;
 
 app.get("/", (req,res) => {
-    res.send("hello world")
+    res.send("hello world, I'm getting a hang on this")
 })
 
+app.get("/ved", (req,res) => {
+    res.send("/ved also has some important stuff")
+})
 app.listen(port, () => {
     console.log(`App started at ${port}`);
 })
