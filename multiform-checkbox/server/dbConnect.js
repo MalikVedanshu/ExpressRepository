ed08259ff05ba7476e5cc85c595e81764ff2b318
malikvedanshu <malikvedanshu757@outlook.com>
2022-08-23 08:49:05 +0000

    setting up dbConnect and app.js

---
+++ multiform-checkbox/server/dbConnect.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+
+async function dbConnect() {
+    try {
+        await mongoose.connect('mongodb+srv://docexp:ze54xsuw7AlBJvdm@cluster0.oqm2c.mongodb.net/test')
+        console.log('Mongo DB is connected');
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+
+dbConnect();
